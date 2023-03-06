b104d67f875144984fa281e67d22ae9babb73796
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-03-06 17:08:22 +0530

    added: tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex h-screen justify-center flex-col items-center">
+      <h1 className="text-3xl">Hello World</h1>
+      <p>This is where the magic happens!</p>
+    </div>
+  );
+}
+
+export default App;
