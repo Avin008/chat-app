3799e41f244f76c5b48a451df5d1bb707b903102
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-03-07 18:44:05 +0530

    added: index page

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,38 @@
+import { Link } from "react-router-dom";
 const Root = () => {
-  return <div>root layout</div>;
+  return (
+    <div className="w-11/12 rounded-md h-80 mx-auto mt-8 grid grid-cols-2">
+      <div className="flex px-3 flex-col justify-center">
+        <h1 className="text-2xl font-medium">
+          Meet your Personal Chat A.I
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Rerum.
+        </p>
+        <div className="flex gap-3 mt-5">
+          <Link
+            className="py-1 px-4 border text-white font-medium bg-[#2F2E41] rounded-md"
+            to="/signup"
+          >
+            Join Now
+          </Link>
+          <Link
+            className="py-1 px-4 border border-black font-medium rounded-md"
+            to="/login"
+          >
+            Already had an Account?
+          </Link>
+        </div>
+      </div>
+      <div className="relative rounded">
+        <img
+          className="absolute rounded-md h-full w-full object-cover"
+          src="https://notation1.vercel.app/notes.svg"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Root;
