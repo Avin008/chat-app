ac87603461ef92cc734712c855b082c58daa2d9e
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-03-07 11:01:20 +0530

    added: nav element

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="h-14 border flex items-center border-black px-6 justify-between">
+    <nav className="h-14 border flex items-center text-[#34312D] border-black px-6 justify-between">
       <div className="flex items-center">
         <span className="text-3xl">☀️</span>
         <span className="text-2xl font-semibold">
