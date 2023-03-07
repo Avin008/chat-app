dbc70ebc34398bc41ab514d21e6270187d40cc51
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-03-08 00:59:46 +0530

    feat: ref-focus on message send

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 const MessageBox = ({
   setMessageFunc,
 }: {
   setMessageFunc: (message: string) => void;
 }) => {
   const [message, setMessage] = useState("");
+  const messageRef = useRef<any>(null);
 
   return (
     <div className="border fixed flex items-center bottom-5 right-40 bg-[#FFFFFF] mx-auto border-black w-1/2 h-12">
@@ -19,12 +20,14 @@ const MessageBox = ({
         ) => {
           setMessage(e.target.value);
         }}
+        ref={messageRef}
       ></textarea>
       <button
         className="font-medium"
         onClick={() => {
           setMessageFunc(message);
           setMessage("");
+          messageRef.current.focus();
         }}
       >
         SEND
